a5475e26f08265a0918d3ea30e8176b5a4d5f2f0
Levi Saravia <20230170@ricaldone.edu.sv>
2024-08-15 15:42:37 -0600

    Crud 70% terminado con guardar, mostrar y eliminar terminados

--- src/controlador/ctrlMenu.java
+++ src/controlador/ctrlMenu.java
@@ -1,13 +1,85 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package controlador;
 
-/**
- *
- * @author Estudiante
- */
-public class ctrlMenu {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import modelo.Menu;
+import vista.frmMenu;
+
+public class ctrlMenu implements MouseListener, KeyListener{
+    
+    private Menu ModeloMenu;
+    private frmMenu vistaMenu;
+    
+    public ctrlMenu(Menu menu, frmMenu frmmenu){
+    
+       this.ModeloMenu = menu;
+       this.vistaMenu = frmmenu;
+       
+       
+       
+       //Siempre poner todos los botones que vamos a ocupar
+       frmmenu.btnGuardar.addMouseListener(this);
+       frmmenu.jtbMenu.addMouseListener(this);
+       
+       ModeloMenu.Mostrar(vistaMenu.jtbMenu);
+           
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        
+        if(e.getSource()== vistaMenu.btnGuardar){
+        
+            ModeloMenu.setNombre(vistaMenu.txtNombre.getText());
+            ModeloMenu.setPrecio(Double.parseDouble(vistaMenu.txtPrecio.getText()));
+            ModeloMenu.setIngredientes(vistaMenu.txtIngrediente.getText());
+            
+            ModeloMenu.Guardar();
+            ModeloMenu.Mostrar(vistaMenu.jtbMenu);
+        }
+        
+        if(e.getSource()== vistaMenu.btnEliminar){
+            ModeloMenu.Eliminar(vistaMenu.jtbMenu);
+            ModeloMenu.Mostrar(vistaMenu.jtbMenu);
+        }
+        
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+        
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        
+    }
     
 }
--- src/modelo/Menu.java
+++ src/modelo/Menu.java
@@ -1,13 +1,111 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package modelo;
 
-/**
- *
- * @author Estudiante
- */
+import java.sql.*;
+import java.util.UUID;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
 public class Menu {
+    
+    String UUID_Menu;
+    String nombre;
+    double precio;
+    String ingredientes;
+
+    public String getUUID_Menu() {
+        return UUID_Menu;
+    }
+
+    public void setUUID_Menu(String UUID_Menu) {
+        this.UUID_Menu = UUID_Menu;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+    
+    public void Guardar() {
+        //Creamos una variable igual a ejecutar el método de la clase de conexión
+        Connection conexion = ClaseConexion.getConexion();
+        try {
+            //Creamos el PreparedStatement que ejecutará la Query
+            PreparedStatement addMenu = conexion.prepareStatement("INSERT INTO tbMenu(UUID_menu, nombre, precio, ingredientes) VALUES (?, ?, ?, ?)");
+            //Establecer valores de la consulta SQL
+            addMenu.setString(1, UUID.randomUUID().toString());
+            addMenu.setString(2, getNombre());
+            addMenu.setDouble(3, getPrecio());
+            addMenu.setString(4, getIngredientes());
+ 
+            //Ejecutar la consulta
+            addMenu.executeUpdate();
+ 
+        } catch (SQLException ex) {
+            System.out.println("este es el error en el modelo:metodo guardar " + ex);
+        }
+    }
+    
+    public void Mostrar(JTable tabla) {
+        //Creamos una variable de la clase de conexion
+        Connection conexion = ClaseConexion.getConexion();
+        //Definimos el modelo de la tabla
+        DefaultTableModel modeloTacos = new DefaultTableModel();
+        modeloTacos.setColumnIdentifiers(new Object[]{"UUID_menu", "nombre", "precio", "ingredientes"});
+        try {
+            //Creamos un Statement
+            Statement statement = conexion.createStatement();
+            //Ejecutamos el Statement con la consulta y lo asignamos a una variable de tipo ResultSet
+            ResultSet rs = statement.executeQuery("SELECT * FROM tbMenu");
+            //Recorremos el ResultSet
+            while (rs.next()) {
+                //Llenamos el modelo por cada vez que recorremos el resultSet
+                modeloTacos.addRow(new Object[]{rs.getString("UUID_menu"), 
+                    rs.getString("nombre"), 
+                    rs.getDouble("precio"), 
+                    rs.getString("ingredientes")});
+            }
+            //Asignamos el nuevo modelo lleno a la tabla
+            tabla.setModel(modeloTacos);
+        } catch (Exception e) {
+            System.out.println("Este es el error en el modelo, metodo mostrar " + e);
+        }
+    }
+    
+    public void Eliminar(JTable tabla) {
+        //Creamos una variable igual a ejecutar el método de la clase de conexión
+        Connection conexion = ClaseConexion.getConexion();
+ 
+        //obtenemos que fila seleccionó el usuario
+        int filaSeleccionada = tabla.getSelectedRow();
+        //Obtenemos el id de la fila seleccionada
+        String miId = tabla.getValueAt(filaSeleccionada, 0).toString();
+        //borramos 
+        try {
+            PreparedStatement deleteEstudiante = conexion.prepareStatement("delete from tbMenu where UUID_menu = ?");
+            deleteEstudiante.setString(1, miId);
+            deleteEstudiante.executeUpdate();
+        } catch (Exception e) {
+            System.out.println("este es el error metodo de eliminar" + e);
+        }
+    }
     
 }
